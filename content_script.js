ae33334512e2a4c6b4e8684a2e4f240c6f267f56
Massimo Bedini <bedini.massimo@gmail.com>
2017-11-23 10:56:27 +0100

    small fixes

--- content_script.js
+++ content_script.js
@@ -7,8 +7,8 @@ temp0=newsapp_node[0].shadowRoot;
 ironlazypages_node=temp0.querySelectorAll('iron-lazy-pages')[0];
 newsarticle_node=ironlazypages_node.querySelectorAll('news-article')[0];
 divampdochost_node=newsarticle_node.shadowRoot.querySelectorAll('div.amp-doc-host')[0];
-body_node=divampdochost_node.shadowRoot.body;
-article_node=body_node.querySelectorAll('div.detail-article_body')[0];
+temp1=divampdochost_node.shadowRoot;
+article_node=temp1.querySelectorAll('div.detail-article_body')[0]
 
 targetremove_node=article_node.querySelectorAll('[amp-access="NOT (showContent)"]')[0];
 targetfix_node=article_node.querySelectorAll('[amp-access="showContent"]')[0];
@@ -16,5 +16,5 @@ targetfix_node=article_node.querySelectorAll('[amp-access="showContent"]')[0];
 // Fixes the nodes
 targetremove_node.parentNode.removeChild(targetremove_node);
 targetfix_node.removeAttribute('amp-access-hide');
-}, 2200);
+}, 4000);
 

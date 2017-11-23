47f90688b6ea8acc8f0ee94e21052a176483d9b6
Massimo Bedini <bedini.massimo@gmail.com>
2017-11-23 02:16:41 +0100

    fully working release

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # riptherep
 RiptheRep Chrome Extension
+
+This extension tries to unlock the content of the articles on https://rep.repubblica.it.
+
+TODO:
+- when the user moves from a page to another, the page must be reloaded in order to remove the paywall
+- find another way to inject the script when the page is loaded, setTimeOut() might not work for everyone
+- find a safer way to select the right nodes to edit
--- content_script.js
+++ content_script.js
@@ -1,4 +1,22 @@
-<script>
-var element = document.querySelector('div.paywall-banner');
-element.parentElement.removeChild(element);
-</script>
+setTimeout(function(){
+
+a=document.querySelectorAll('news-app');
+b=a[0].shadowRoot;
+c=b.childNodes[17];
+d=c.childNodes[3];
+e=d.shadowRoot.childNodes[2];
+f=e.childNodes[1];
+g=f.childNodes[1];
+h=g.childNodes[1];
+i=h.shadowRoot.childNodes[11];
+l=i.childNodes[8];
+m=l.childNodes[0];
+n=m.childNodes[3];
+o=n.childNodes[0];
+targetremove=o.childNodes[0];
+targetfix=o.childNodes[1];
+
+targetremove.parentNode.removeChild(targetremove);
+targetfix.removeAttribute('amp-access-hide');
+}, 2200);
+

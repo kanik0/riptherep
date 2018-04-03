5df8917a2305bdcb919f7fc0248ceb23a08a1964
bretello <bretello>
2018-04-04 01:52:32 +0200

    Using window.onload() instead of the timer

--- content_script.js
+++ content_script.js
@@ -1,20 +1,19 @@
 // Waits until the page is fully loaded
-setTimeout(function(){
-
-// Moves to the nodes that must be fixed
-newsapp_node=document.querySelectorAll('news-app');
-temp0=newsapp_node[0].shadowRoot;
-ironlazypages_node=temp0.querySelectorAll('iron-lazy-pages')[0];
-newsarticle_node=ironlazypages_node.querySelectorAll('news-article')[0];
-divampdochost_node=newsarticle_node.shadowRoot.querySelectorAll('div.amp-doc-host')[0];
-temp1=divampdochost_node.shadowRoot;
-article_node=temp1.querySelectorAll('div.detail-article_body')[0]
-
-targetremove_node=article_node.querySelectorAll('[amp-access="NOT (showContent)"]')[0];
-targetfix_node=article_node.querySelectorAll('[amp-access="showContent"]')[0];
-
-// Fixes the nodes
-targetremove_node.parentNode.removeChild(targetremove_node);
-targetfix_node.removeAttribute('amp-access-hide');
-}, 4000);
+window.onload = function(){
+    // Moves to the nodes that must be fixed
+    newsapp_node = document.querySelectorAll('news-app');
+    temp0 = newsapp_node[0].shadowRoot;
+    ironlazypages_node = temp0.querySelectorAll('iron-lazy-pages')[0];
+    newsarticle_node = ironlazypages_node.querySelectorAll('news-article')[0];
+    divampdochost_node = newsarticle_node.shadowRoot.querySelectorAll('div.amp-doc-host')[0];
+    temp1 = divampdochost_node.shadowRoot;
+    article_node = temp1.querySelectorAll('div.detail-article_body')[0]
+    
+    targetremove_node = article_node.querySelectorAll('[amp-access = "NOT (showContent)"]')[0];
+    targetfix_node = article_node.querySelectorAll('[amp-access = "showContent"]')[0];
+    
+    // Fixes the nodes
+    targetremove_node.parentNode.removeChild(targetremove_node);
+    targetfix_node.removeAttribute('amp-access-hide');
+};
 
